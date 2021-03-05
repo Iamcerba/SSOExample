9b42cdda09c9a8d4be92c32be4ec8bdfec718e11
Alexander Shabunevich <alexander.shabunevich@gmail.com>
2021-03-05 13:00:52 +0300

    Update.

---
+++ UsersService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UsersService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
